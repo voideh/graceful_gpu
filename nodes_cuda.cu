62d3e38cfb1b47657169e262f6919fd89e712be2
Jake <jakedlopez@gmail.com>
2016-11-25 18:43:30 -0500

    parallel edge calcuations are now working...

--- nodes_cuda.cu
+++ nodes_cuda.cu
@@ -1,63 +1,103 @@
-#include<iostream>
+#include <stdlib.h>
+#include <iostream>
 #include <algorithm>
 using namespace std;
-const int NUMNODES = 8;
 
 int factorial(int n)
 {
   return (n == 0 || n == 1) ? 1 : factorial(n-1) * n;
 }
-const int NUMPERMS = factorial(NUMNODES);
 
-__global__ void gpu_check_edges(int *c, int *l, int * stop, int* store)
+__global__ void check_gracefulness(int *perms, int *children, int *stops, int *graceful, int *temp, int NUMNODES, int NUMPERMS)
 {
+    // will start at begining of node permutation
+    int element = (blockIdx.x * blockDim.x + threadIdx.x) * NUMNODES; 
+    int total = NUMNODES * NUMPERMS;
+    int temp_counter = 0;
+    int last_index = 0;
+    if(element < total)
+    {
+        for(int i = element; i < element + NUMNODES; i++)
+        {
+            if(stops[i % NUMNODES] != -1)
+            {
+                for(int j = (last_index == 0) ? 0 : last_index+1; j<=stops[i % NUMNODES]; j++)
+                    temp[element + temp_counter++] = abs(perms[i] - perms[children[j] + element]);
+                last_index = stops[i % NUMNODES];
+            }
+        }
+    }
 }
 
-void prepare_gpu(int NUMPERMS, int NUMNODES,  int c[], int p[NUMPERMS][NUMNODES] , int results[], int s[])
+void execute_gpu(int perms[], int children[], int stops[], int graceful_labels[], int temp[], int NUMNODES, int NUMPERMS)
 {
-    int* graceful_labels; 
-    int* children;
-    int* permutations;
-    int* stop;
-    size_t pitch;
-    int numCores = NUMPERMS / 768 + 1;
+    int *d_perms, *d_children, *d_graceful_labels, *d_stops, *temp_calc;
+
+    const size_t perm_size = NUMNODES*NUMPERMS*sizeof(int);
+    const size_t child_size = (NUMNODES-1)*sizeof(int);
+    const size_t stop_size = NUMNODES*sizeof(int);
+    const size_t label_size = NUMPERMS*sizeof(int);
+
+    int numCores = (NUMNODES * NUMPERMS)/ 768 + 1;
     int numThreads = 1024;
-    // allocate mem
-    cudaMalloc(&graceful_labels, sizeof(int) * NUMPERMS);
-    cudaMalloc(&children, sizeof(int) * NUMNODES-1);
-    cudaMallocPitch((void**)&permutations, &pitch, NUMNODES * sizeof(int), NUMPERMS);
-    cudaMalloc(&stop, sizeof(int) * NUMNODES);
-    // copy over stuff
-    cudaMemcpy(children, c, sizeof(int) * NUMNODES-1, cudaMemcpyHostToDevice);
-    cudaMemcpy2D(permutations, pitch, p, pitch, NUMNODES * sizeof(int), NUMPERMS, cudaMemcpyHostToDevice);
-    cudaMemcpy(stop, s, sizeof(int) * NUMNODES, cudaMemcpyHostToDevice);
-    // run code
-    gpu_check_edges<<numCores, numThreads>>(children, permutations, stop, graceful_labels);
-    cudaMemcpy(results, graceful_labels, sizeof(int)*NUMPERMS, cudaMemcpyDeviceToHost);
-    cudaFree(&graceful_labels);
-    cudaFree(permutations);
-    cudaFree(&stop);
-    cudaFree(&children);
+
+    cudaMalloc(&d_perms, perm_size);
+    cudaMalloc(&temp_calc, perm_size);
+    cudaMalloc(&d_children, child_size);
+    cudaMalloc(&d_stops, stop_size);
+    cudaMalloc(&d_graceful_labels, label_size);
+
+    cudaMemcpy(d_perms, perms, perm_size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_children, children, child_size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_stops, stops, stop_size, cudaMemcpyHostToDevice);
+
+    check_gracefulness<<<numCores, numThreads>>>(d_perms,
+            d_children,
+            d_stops,
+            d_graceful_labels,
+            temp_calc,
+            NUMNODES,
+            NUMPERMS);
+    cudaMemcpy(temp, temp_calc, perm_size, cudaMemcpyDeviceToHost);
+
+    cudaFree(&d_perms);
+    cudaFree(&d_stops);
+    cudaFree(&d_children);
+    cudaFree(&d_graceful_labels);
+    cudaFree(&temp_calc);
 }
 
 int main()
 {
-    int NUMNODES = 8;
-    int NUMPERMS = factorial(NUMNODES);
-    int children[NUMNODES-1];
-    int labels[NUMNODES];
-    int results[NUMPERMS];
-    int stop[] = {1, 3, 4, -1, 6, -1 ,-1 ,-1};
-    int permutations[NUMPERMS][NUMNODES];
+    const int NUMNODES = 5;
+    const int NUMPERMS = factorial(NUMNODES);
+    int children[NUMNODES-1], perms[NUMPERMS*NUMNODES], graceful_labels[NUMPERMS], labels[NUMNODES];
+    int temp[NUMPERMS*NUMNODES-1];
+    int stops[] = {1, 3, -1, -1, -1};
+    // generate both children and label array
     for(int i = 0; i < NUMNODES; i++)
+    {
         labels[i] = i;
-    for(int i = 0; i < NUMNODES; i++)
-        children[i] = i+1;
+        if(i < NUMNODES - 1) children[i] = i+1;
+    }
+
+    // create all permutations of given nodes
     for(int i = 0; i < NUMPERMS; i++)
     {
         for(int j = 0; j < NUMNODES; j++)
-            permutations[i][j] = labels[j];
+        {
+            perms[i*NUMNODES+j] = labels[j];
+            temp[i*NUMNODES+j] = 0;
+        }
+        graceful_labels[i] = -1;
         next_permutation(labels, labels+NUMNODES);
     }
-    return 0;
+    execute_gpu(perms, children, stops, graceful_labels, temp, NUMNODES, NUMPERMS);
+
+    for(int i = 0; i < NUMPERMS; i++)
+    {
+        for(int j = 0; j < NUMNODES-1; j++)
+            cout << temp[i*NUMNODES+j] << " ";
+        cout << endl;
+    }
 }
